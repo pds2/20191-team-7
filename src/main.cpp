0c138036cf0c0e8bc4c3bb8604f2c2f5a6d73a63
GabrielEvan <45538091+GabrielEvan@users.noreply.github.com>
2019-05-26 16:22:59 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <Partida.h>
 
 int main() {
+    /*
     //inicia o jogo
     Jogo jogo = Jogo();
     jogo.carrega_arquivos();
@@ -18,7 +19,15 @@ int main() {
     Personagem b0 = jogo.escolhe_monstro(0);
     Personagem b1 = jogo.escolhe_monstro(1);
     Personagem b2 = jogo.escolhe_monstro(2);
-    
+    */
+    Personagem a0 = Personagem("Jon Snow",10,10,10);
+    Personagem a1 = Personagem("Jorah Mormont",14,8,8);
+    Personagem a2 = Personagem("Daenerys",8,8,14);
+    Personagem b0 = Personagem("Rato",2,2,2);
+    Personagem b1 = Personagem("Orc",8,8,8);
+    Personagem b2 = Personagem("Cthulhu",20,20,500);
+
+
     //Inicia partida com os personagens escolhidos
     Partida partida = Partida(a0, a1, a2, b0, b1, b2);
 
